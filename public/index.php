5e4122e510494943aea4acc79ca82694b5b7f893
EJ Perez <idyey04@gmail.com>
2020-04-22 11:39:36 +0800

    API request test

--- public/index.php
+++ public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "It twerks";
+$apiKey = getenv('API_KEY');
+$q = $_GET['q'] ?? null;
+
+if(!$apiKey || !$q) {
+	die('<h1>It works!</h1>');
+}
+
+$params = http_build_query([
+	'q' => $q,
+	'part' => 'snippet',
+	'key' => $apiKey,
+	'maxResults' => 10,
+	'type' => 'video'
+]);
+
+$response = file_get_contents('https://www.googleapis.com/youtube/v3/search?' . $params);
+
+header('Content-Type: application/json');
+echo json_encode($response);
